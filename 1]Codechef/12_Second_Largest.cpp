6b3b798d871f072ce26f3e50c5e5362b11e7c391
Sukrutiii <97895492+Sukrutiii@users.noreply.github.com>
2022-02-11 15:22:49 +0530

    12

---
+++ 1]Codechef/12_Second_Largest.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+
+while(t--){
+  long long a,b,c;
+  cin>>a>>b>>c;
+  if(a>=b && b>=c || a<=b && b<=c){
+      cout << b << endl;
+    }
+	else if(a>=c && c>=b || a<=c && b>=c){
+        cout << c << endl;
+    }
+	else{
+        cout << a << endl;
+    }
+  
+}
+return 0;
+}
