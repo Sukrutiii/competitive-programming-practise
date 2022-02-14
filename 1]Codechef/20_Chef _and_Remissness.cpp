33e6e2394773dd5fd379ed75f5f15e29b2e520be
Sukrutiii <97895492+Sukrutiii@users.noreply.github.com>
2022-02-14 14:34:01 +0530

    20

---
+++ 1]Codechef/20_Chef _and_Remissness.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int a,b;
+  cin>>a>>b;
+  if(a>b){
+    cout<<a<<endl;
+    cout<<a+b<<endl;
+  }else{
+      cout<<b<<endl;
+      cout<<a+b<<endl;
+  }
+}
+return 0;
+}
