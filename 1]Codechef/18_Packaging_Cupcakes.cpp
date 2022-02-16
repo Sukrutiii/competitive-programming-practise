1d23c81e79cb5c2f86824de779466bae331ff480
Sukruti Thakare <suk613939@gmail.com>
2022-02-16 11:43:24 +0530

    21

---
+++ 1]Codechef/18_Packaging_Cupcakes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    cout<<n/2+1<<"\n";
+}
+return 0;
+}
---
+++ 1]Codechef/21_RCB_and_Playoffs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ 
+int main(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int t;cin>>t;
+while(t--){
+  int x,y,z;
+  cin>>x>>y>>z;
+  if(y-x>z*2){
+      cout<<"no"<<endl;
+  }
+  else{
+      cout<<"yes"<<endl;
+  }
+}
+return 0;
+}
