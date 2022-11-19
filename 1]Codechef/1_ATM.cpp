2721780727d8c3b95554f3969321de98d93fe0b4
Sukrutiii <suk613939@gmail.com>
2022-11-19 18:45:50 +0530

    1

--- 1]Codechef/1_ATM.cpp
+++ 1]Codechef/1_ATM.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;
-float b;
-cin>>t>>b;
-if((t%5==0 && b-t-0.5>0)){
-   cout << fixed << b-t-0.5 << endl; 
-}
-else
+    int t;
+    float b;
+    cin >> t >> b;
+    if ((t % 5 == 0 && b - t - 0.5 > 0))
+    {
+        cout << fixed << b - t - 0.5 << endl;
+    }
+    else
     {
         cout << fixed << b << endl;
     }
-return 0;
+    return 0;
 }
