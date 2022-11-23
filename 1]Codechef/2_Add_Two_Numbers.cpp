cc987f0a323c814dc8c8a60d1750b89c6b1b7f7c
Sukrutiii <suk613939@gmail.com>
2022-11-23 18:49:40 +0530

    2

--- 1]Codechef/2_Add_Two_Numbers.cpp
+++ 1]Codechef/2_Add_Two_Numbers.cpp
@@ -1,18 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;cin>>t;
-int a,b;
-while(t--){
-    int sum=0;
-    cin>>a>>b;
-    sum=a+b;
-    cout<<sum<<"\n";
-}
-return 0;
+    int t;
+    cin >> t;
+    int a, b;
+    while (t--)
+    {
+        int sum = 0;
+        cin >> a >> b;
+        sum = a + b;
+        cout << sum << "\n";
+    }
+    return 0;
 }
