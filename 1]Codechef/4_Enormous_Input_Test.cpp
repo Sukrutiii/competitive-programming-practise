a5819131d292b57a106f22c4c6f76adbeddbea1e
Sukrutiii <suk613939@gmail.com>
2022-11-25 18:51:47 +0530

    4

--- 1]Codechef/4_Enormous_Input_Test.cpp
+++ 1]Codechef/4_Enormous_Input_Test.cpp
@@ -1,26 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-int t;
-    cin>>t;
-    while(t--)
+    int t;
+    cin >> t;
+    while (t--)
     {
         int n;
-        cin>>n;
+        cin >> n;
         int sum = 0;
-        while(n!=0)
+        while (n != 0)
         {
-            int r = n%10;
-            sum+= r;
+            int r = n % 10;
+            sum += r;
             n /= 10;
         }
 
-        cout<<sum<<endl;
+        cout << sum << endl;
     }
-return 0;
+    return 0;
 }
