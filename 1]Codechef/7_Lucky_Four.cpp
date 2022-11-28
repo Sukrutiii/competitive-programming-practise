4906a43d83642072d67dfa3b1c8f1fc9aace6aa0
Sukrutiii <suk613939@gmail.com>
2022-11-28 18:56:33 +0530

    7

--- 1]Codechef/7_Lucky_Four.cpp
+++ 1]Codechef/7_Lucky_Four.cpp
@@ -1,21 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
 	// your code goes here
 	int t;
 	cin >> t;
-	while(t--){
-	    int i;
-	    cin >> i;
-	    int count=0;
-	    for(int j=i;j>0;){
-	        if(j%10==4){
-	            count=count+1;
-	        }
-	        j=j/10;
-	    }
-	    cout << count<< endl;
+	while (t--)
+	{
+		int i;
+		cin >> i;
+		int count = 0;
+		for (int j = i; j > 0;)
+		{
+			if (j % 10 == 4)
+			{
+				count = count + 1;
+			}
+			j = j / 10;
+		}
+		cout << count << endl;
 	}
 	return 0;
 }
