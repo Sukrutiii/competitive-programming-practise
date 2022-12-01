057cfa1cd07393f13c0722e91993bb1e0db7b03b
Sukrutiii <suk613939@gmail.com>
2022-12-01 19:53:33 +0530

    10

--- 1]Codechef/10_Small_factorials.cpp
+++ 1]Codechef/10_Small_factorials.cpp
@@ -2,20 +2,20 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
 
-int main() {
-	
 	int t;
-	cin>>t;
-	while(t--)
+	cin >> t;
+	while (t--)
 	{
-	    int n;
-	    cin>>n;
-	    int fact=1;
-	    for(int i=n;i>0;i--)
-	     fact=fact*i;
-	    cout<<fact<<endl; 
+		int n;
+		cin >> n;
+		int fact = 1;
+		for (int i = n; i > 0; i--)
+			fact = fact * i;
+		cout << fact << endl;
 	}
-	
+
 	return 0;
 }
