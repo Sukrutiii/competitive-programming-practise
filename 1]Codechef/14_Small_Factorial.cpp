408ac46c194e5f2d53cc2373f0a0834302409cd8
Sukruti Thakare <97895492+Sukrutiii@users.noreply.github.com>
2022-12-04 17:48:49 +0530

    14

--- 1]Codechef/14_Small_Factorial.cpp
+++ 1]Codechef/14_Small_Factorial.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -18,25 +17,4 @@ while(t--){
 	cout<<fact<<endl; 
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int n,i;
-  cin>>n;
-  int fact=1;
-	for(int i=n;i>0;i--){
-    fact=fact*i;
-  }
-	cout<<fact<<endl; 
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
