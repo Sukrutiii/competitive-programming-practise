353f7705b5ecc4e8dd4003e7e32c87610c091161
Sukruti Thakare <97895492+Sukrutiii@users.noreply.github.com>
2022-12-05 21:05:41 +0530

    15

--- 1]Codechef/15_Chef_And_Operators.cpp
+++ 1]Codechef/15_Chef_And_Operators.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -22,29 +21,4 @@ while(t--){
   
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int a,b;
-  cin>>a>>b;
-  if(a<b){
-      cout<<"<"<<endl;
-  }
-  else if (a>b){
-    cout<<">"<<endl;
-  }else{
-      cout<<"="<<endl;
-  }
-  
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
