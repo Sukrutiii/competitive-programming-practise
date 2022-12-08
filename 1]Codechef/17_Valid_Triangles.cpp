dcc1360695c83c403a68b76cf85bda58712d4734
Sukrutiii <suk613939@gmail.com>
2022-12-08 12:02:16 +0530

    17

--- 1]Codechef/17_Valid_Triangles.cpp
+++ 1]Codechef/17_Valid_Triangles.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -19,26 +18,5 @@ while(t--){
   }
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(a==0 || b==0 || c==0 || a+b+c!=180){
-    cout << "NO" << endl;
-  }
-  else{
-    cout << "YES" << endl; 
-  }
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
