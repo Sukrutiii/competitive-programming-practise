79b7b6c2bdd047021565a2458aef1ee249cb23cd
Sukrutiii <suk613939@gmail.com>
2022-12-09 11:30:44 +0530

    18

--- 1]Codechef/18_Packaging_Cupcakes.cpp
+++ 1]Codechef/18_Packaging_Cupcakes.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -14,21 +13,5 @@ while(t--){
     cout<<n/2+1<<"\n";
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-    int n;
-    cin>>n;
-    cout<<n/2+1<<"\n";
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
