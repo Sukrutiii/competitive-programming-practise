8f72049ebe49ca48bff85f046ab6b8b65313657b
Sukrutiii <suk613939@gmail.com>
2022-12-09 11:31:41 +0530

    19

--- 1]Codechef/19_Decrement_OR_Increment.cpp
+++ 1]Codechef/19_Decrement_OR_Increment.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -18,25 +17,5 @@ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
   }
 
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-  int n,a;
-  cin>>n;
-  if(n%4==0){
-      a=n+1;
-      cout<<a;
-  }else{
-      a=n-1;
-      cout<<a;
-  }
-
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
