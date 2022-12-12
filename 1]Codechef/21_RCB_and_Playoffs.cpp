c5453265240c2f5c12e604e5960232bcf861e4e2
Sukrutiii <suk613939@gmail.com>
2022-12-12 11:33:58 +0530

    21

--- 1]Codechef/21_RCB_and_Playoffs.cpp
+++ 1]Codechef/21_RCB_and_Playoffs.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -19,26 +18,5 @@ while(t--){
   }
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int x,y,z;
-  cin>>x>>y>>z;
-  if(y-x>z*2){
-      cout<<"no"<<endl;
-  }
-  else{
-      cout<<"yes"<<endl;
-  }
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
