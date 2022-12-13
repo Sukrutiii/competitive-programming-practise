45729f6644d4bd193af66139fa5be7e2313e251b
Sukrutiii <suk613939@gmail.com>
2022-12-13 11:38:40 +0530

    24

--- 1]Codechef/24_Chef_On_Island.cpp
+++ 1]Codechef/24_Chef_On_Island.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -18,25 +17,5 @@ int main() {
          cout<<"NO"<<endl;
 	}
 	return 0;
-=======
-#include <iostream>
-using namespace std;
 
-int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    float x,y,x1,y1,a,b;
-	    
-	    cin>>x>>y>>x1>>y1;
-	    int d;
-	    cin>>d; 
-	    if(min(x/x1,y/y1)>=d)
-        cout<<"YES"<<endl;
-	    else
-         cout<<"NO"<<endl;
-	}
-	return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
