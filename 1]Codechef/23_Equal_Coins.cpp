7972d65810679862f514adc0093f1ea8a49d0208
Sukrutiii <suk613939@gmail.com>
2022-12-15 11:36:18 +0530

    23

--- 1]Codechef/23_Equal_Coins.cpp
+++ 1]Codechef/23_Equal_Coins.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -26,33 +25,5 @@ while(t--){
     }
 }
 return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
- 
-int main(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-int t;cin>>t;
-while(t--){
-  int x, y;
-    cin >> x >> y;
-        
-    if (x == 0 && y % 2 == 0){
-        cout << "Yes" << endl;
-    }
-    else if (x == 0 && y % 2 != 0){
-        cout << "NO" << endl;
-    }
-    else if ((x + (2 * y)) % 2 ==0){
-        cout << "YES" << endl;
-    }
-    else {
-        cout << "NO" << endl;
-    }
-}
-return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
