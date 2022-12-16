11f0dc2b10998fb4d0183f85244f51d20cb744ff
Sukrutiii <suk613939@gmail.com>
2022-12-16 20:12:26 +0530

    25

--- 1]Codechef/25_Coins_And_Triangle.cpp
+++ 1]Codechef/25_Coins_And_Triangle.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,29 +21,5 @@ int main() {
 	    }
 	}
 	return 0;
-=======
-#include <bits/stdc++.h>
-using namespace std;
 
-int main() {
-	int t;
-	cin >> t;
-	while(t--){
-	    int n;
-	    cin >> n;
-	    int sum = 0,count = 0;
-	    for(int i = 1; i<=n; i++){
-	        sum +=i;
-	        count++;
-	        if(sum>n){
-	            cout << count-1 << endl;
-	            break;
-	        }else if(sum == n){
-	            cout << count << endl;
-	            break;
-	        }
-	    }
-	}
-	return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
