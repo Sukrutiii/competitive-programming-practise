e54769a8d39fe644d65ea46b14390473952309a8
Sukrutiii <suk613939@gmail.com>
2022-12-17 20:13:37 +0530

    26

--- 1]Codechef/26_Possible_Victory.cpp
+++ 1]Codechef/26_Possible_Victory.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -19,26 +18,4 @@ int main() {
         }
 
         return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-	int a,b,c,d;
-	
-	cin>>a>>b>>c;
-	
-	d=(20-b)*36;
-	
-	if(a<d+c)
-        {
-                cout<<"YES"<<endl;
-        }
-        else
-        {
-                cout<<"NO"<<endl;
-        }
-
-        return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
