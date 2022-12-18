bedd44a7ae010635441512bdc2996abc27e04a9b
Sukrutiii <suk613939@gmail.com>
2022-12-18 20:16:15 +0530

    27

--- 1]Codechef/27_Lazy_Chef.cpp
+++ 1]Codechef/27_Lazy_Chef.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,18 +10,5 @@ int main() {
 	    cout << min(a+c,b*a) <<endl;
 	}
 	return 0;
-=======
-#include <bits/stdc++.h>
-using namespace std;
 
-int main() {
-	int t;
-	cin >> t;
-	while(t--){
-	    int a,b,c;
-	    cin >> a >> b >> c;
-	    cout << min(a+c,b*a) <<endl;
-	}
-	return 0;
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
 }
