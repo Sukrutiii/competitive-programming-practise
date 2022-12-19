1a617580378405bcb624a311bba2bf39c761e9ee
Sukrutiii <suk613939@gmail.com>
2022-12-19 20:17:18 +0530

    28

--- 1]Codechef/28_Cyclic_Quadrilateral.cpp
+++ 1]Codechef/28_Cyclic_Quadrilateral.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -21,27 +20,4 @@ int main() {
     return 0;
     
 }
-=======
-#include <iostream>
-using namespace std;
 
-int main() {
-	int t; 
-	cin>>t;
-    while (t--)
-    {
-        int x,y,a,b,c,d;
-        cin>>a>>b>>c>>d;
-        y=a+c;
-        x=b+d;
-        if(y==180 and x==180){
-            cout<<"YES"<<endl;
-        }
-        else{
-            cout<<"NO"<<endl;
-        }
-    }
-    return 0;
-    
-}
->>>>>>> 5d34dce3d75f4a97b4ef226477f2c23687ef0738
